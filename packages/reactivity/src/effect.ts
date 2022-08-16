887c5ce4d0a959fa296e3a3a468c9b1f7ee66e7a
zhouxingting@newrank.cn <1404203081@qq.com>
2022-08-16 11:30:45 +0800

    feat: 初始化effect

---
+++ packages/reactivity/src/effect.ts
@@ -0,0 +1,106 @@
+import { extend } from "@mini-vue/shared";
+
+class ReactiveEffect {
+  private _fn: any;
+  public scheduler: Function | undefined;
+  deps = [];
+  active = true;
+  onStop: any;
+  constructor(fn, scheduler?) {
+    this._fn = fn;
+    // this.scheduler = scheduler;
+  }
+  run() {
+    /** 可以开始收集依赖 */
+    activeEffect = this;
+    shouldTrack = true;
+
+    if (this.onStop) {
+      this.onStop();
+    }
+    const result = this._fn();
+
+    /** 重置 */
+    activeEffect = undefined;
+    shouldTrack = false;
+
+    return result;
+  }
+  stop() {
+    if (this.active) {
+      cleanupEffect(this);
+      this.active = false;
+    }
+  }
+}
+
+function cleanupEffect(effect) {
+  effect.deps.forEach((dep) => {
+    dep.delete(effect);
+  });
+}
+
+const targetMap = new Map();
+
+/** 判断是否正在收集依赖 */
+export function isTracking() {
+  return shouldTrack && !!activeEffect;
+}
+
+/** 收集依赖 */
+export function track(target, key) {
+  /** 收集过一次依赖则return，不再收集当前依赖 */
+  if (!isTracking()) return;
+
+  /** 存储关系是target => key => fn */
+  let depsMap = targetMap.get(target);
+  if (!depsMap) {
+    depsMap = new Map();
+    targetMap.set(target, depsMap);
+  }
+  let deps = depsMap.get(key);
+  if (!deps) {
+    deps = new Set();
+    depsMap.set(key, deps);
+  }
+  deps.add(activeEffect);
+  /** 存储当前收集的依赖 */
+  activeEffect?.deps.push(deps);
+}
+
+/** 执行依赖 */
+export function trigger(target, key) {
+  const depsMap = targetMap.get(target);
+  const deps = depsMap.get(key);
+  if (!depsMap || !deps) return;
+  for (let effect of deps) {
+    if (effect.scheduler) {
+      effect.scheduler();
+    } else {
+      effect.run();
+    }
+  }
+}
+
+/** 全局变量，存储当前的Effect的fn */
+let activeEffect;
+let shouldTrack = false;
+
+export function effect(fn, options: any = {}) {
+  // const _effect = new ReactiveEffect(fn, options.scheduler);
+  const _effect = new ReactiveEffect(fn);
+
+  extend(_effect, options);
+
+  _effect.run();
+
+  let runner: any = _effect.run.bind(_effect);
+  runner.effect = _effect;
+
+  return runner;
+}
+
+/** 清空收集的依赖 */
+export function stop(runner) {
+  runner.effect.stop();
+}
---
+++ packages/reactivity/src/reactive.ts
@@ -0,0 +1,18 @@
+import { track, trigger } from "./effect";
+
+export function reactive(raw) {
+  return new Proxy(raw, {
+    get: function (target, key) {
+      const res = Reflect.get(target, key);
+      //TODO 收集依赖
+      track(target, key);
+      return res;
+    },
+    set: function (target, key, value) {
+      const res = Reflect.set(target, key, value);
+      //TODO 执行依赖
+      trigger(target, key);
+      return res;
+    },
+  });
+}
